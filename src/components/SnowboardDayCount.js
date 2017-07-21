beeb675888188c9676374cf138722a0b139e82f9
Stanislav Maksarov <maksarov.s@gmail.com>
2017-07-20 22:58:03 -0700

    Improve syntax by using stateless functional components

--- src/components/SnowboardDayCount.js
+++ src/components/SnowboardDayCount.js
@@ -1,36 +1,30 @@
-import { Component } from 'react';
 import '../stylesheets/ui.scss'
 
-export class SnowboardDayCount extends Component{
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + '%')
-  }
-
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  }
-
-  render() {
-    return (
-      <div className="snowboard-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span>days</span>
-        </div>
-        <div className="powder-days">
-          <span>{this.props.powder}</span>
-          <span>days</span>
-        </div>
-        <div className="backcountry-days">
-          <span>{this.props.backcountry}</span>
-          <span>days</span>
-        </div>
-        <div>
-          <span>
-            {this.calcGoalProgress(this.props.total, this.props.goal)}
-          </span>
-        </div>
-      </div>
-    )
-  }
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total/goal)
+}
+
+export const SnowboardDayCount = ({total, powder, backcountry, goal}) => 
+  <div className="snowboard-day-count">
+    <div className="total-days">
+      <span>{total}</span>
+      <span>days</span>
+    </div>
+    <div className="powder-days">
+      <span>{powder}</span>
+      <span>days</span>
+    </div>
+    <div className="backcountry-days">
+      <span>{backcountry}</span>
+      <span>days</span>
+    </div>
+    <div>
+      <span>
+        {calcGoalProgress(total, goal)}
+      </span>
+    </div>
+  </div>
