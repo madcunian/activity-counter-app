ad51e1a3b0b66f0dd4fb4b9c9e3596f8033cfbfa
Stanislav Maksarov <maksarov.s@gmail.com>
2017-07-20 23:19:25 -0700

    Create array of objects in SnowboardDayCount.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SnowboardDayCount } from './components/SnowboardDayCount'
+import { SnowboardDayList } from './components/SnowboardDayList'
 
 window.React = React
 
 render(
-	<SnowboardDayCount 
-				total={50}
-				powder={20}
-				backcountry={10}
-				goal={100}/>,
+	<SnowboardDayList days={
+		[
+			{
+				resort: "Cypress Mountain",
+				date: new Date("1/2/2017"),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Grouse Mountain",
+				date: new Date("3/28/2017"),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Mount Seymour",
+				date: new Date("1/2/2017"),
+				powder: false,
+				backcountry: true
+			}
+		]
+	}/>,
 	document.getElementById('react-container')
 )
