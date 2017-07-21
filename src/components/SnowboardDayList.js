da2fdaaa4d3633210d23ea934855a4a6d7b33967
Stanislav Maksarov <maksarov.s@gmail.com>
2017-07-20 23:47:10 -0700

    Create SnowboardDayList.js child component

---
+++ src/components/SnowboardDayList.js
@@ -0,0 +1,26 @@
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/md/ac-unit'
+import Calendar from 'react-icons/lib/fa/calendar'
+import { SnowboardDayRow } from './SnowboardDayRow'
+
+export const SnowboardDayList = ({days}) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Resort</th>
+        <th>Powder</th>
+        <th>Backcountry</th>
+      </tr>
+    </thead>
+    <tbody>
+      {days.map((day, i) =>
+        <SnowboardDayRow key={i}
+                         resort={day.resort}
+                         date={day.date}
+                         powder={day.powder}
+                         backcountry={day.backcountry} />
+      )}
+    </tbody>
+  </table>
+)
