2b27c3f0280eec92a92b4484045ad5813781f4c6
Stanislav Maksarov <maksarov.s@gmail.com>
2017-07-22 00:39:07 -0700

    Install and incorporate Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
 render(
-	<App />,
+	<Router history={hashHistory}>
+		<Route path="/" component={App} />
+		<Route path="*" component={Whoops404}/>
+	</Router>,
 	document.getElementById('react-container')
 )
